8f1a19f384ef8230a8514ed110dd20c86f391d8b
Annavarapu Sai Jagadeesh <asaijagadeesh15@gmai.com>
2023-10-15 14:56:28 +0530

    changed train_sentence, train_label_id paths to /content/semeval

--- utils.py
+++ utils.py
@@ -1,8 +1,9 @@
 import json
 def get_dataset(type="train", ds="semeval"):
     # texts = json.load(open(f"./{ds}/{type}_sentence.json", "r"))
-    texts = json.load(open(f"/content/RE2/semeval/train_sentence.json","r"))
+    # /content/sample_data
+    texts = json.load(open(f"/content/semeval/train_sentence.json","r"))
     # relations = json.load(open(f"./{ds}/{type}_label_id.json", "r"))
-    relations = json.load(open(f"/content/RE2/semeval/train_label_id.json","r"))
+    relations = json.load(open(f"/content/semeval/train_label_id.json","r"))
     print(f"processing {type} dataset ...")
     return texts, relations
